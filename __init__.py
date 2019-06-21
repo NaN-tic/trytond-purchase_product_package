78708c49512ac79f696ad74d5931b4b637541272
Raimon Esteve <raimon@nan-tic.com>
2019-06-21 12:57:11 +0200

    Not allow change quantity package when find packages in purchase lines #038038

--- __init__.py
+++ __init__.py
@@ -1,11 +1,13 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 from trytond.pool import Pool
+from . import product
 from . import purchase
 
 
 def register():
     Pool.register(
+        product.Package,
         purchase.PurchaseLine,
         module='purchase_product_package', type_='model')
     Pool.register(
