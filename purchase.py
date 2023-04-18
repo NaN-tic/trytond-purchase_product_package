cdf03eaca4c1e8b9c3e275963296d50a7963b6ce
Juanjo Garcia <juanjo.garcia@nan-tic.com>
2023-04-18 13:26:49 +0200

    FIX depends.
    Task #067415

--- purchase.py
+++ purchase.py
@@ -32,14 +32,15 @@ class PurchaseLine(metaclass=PoolMeta):
             'required': Eval('product_has_packages', False),
             'readonly': Eval('purchase_state') != 'draft',
             },
-        depends=['product_template', 'product_has_packages', 'purchase_state'])
+        depends=['product_template', 'product_has_packages', 'purchase_state',
+            'proudct',])
     package_quantity = fields.Integer('Package Quantity',
         states={
             'invisible': ~Eval('product_has_packages', False),
             'required': Eval('product_has_packages', False),
             'readonly': Eval('purchase_state') != 'draft',
             },
-        depends=['product_has_packages', 'purchase_state', 'proudct'])
+        depends=['product_has_packages', 'purchase_state'])
 
     @fields.depends('product_package', 'quantity', 'product_package',
         'product')
