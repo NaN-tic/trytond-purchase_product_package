9573a88e0f6de2ca527daea13caa6c02d64b3528
Àngel Àlvarez <angel@nan-tic.com>
2023-05-31 18:03:07 +0200

    change validation to allow package_quantity with decimals

--- purchase.py
+++ purchase.py
@@ -53,7 +53,7 @@ class PurchaseLine(metaclass=PoolMeta):
                 and Transaction().context.get('validate_package', True)):
             package_quantity = ((self.quantity or 0.0) /
                 self.product_package.quantity)
-            if float(int(package_quantity)) != package_quantity:
+            if float(round(package_quantity,0)) != self.package_quantity:
                 raise UserError(gettext(
                     'purchase_product_package.msg_package_quantity',
                     qty=self.quantity,
