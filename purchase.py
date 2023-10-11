a18c6be5cd976e50c981e608bf674862ecf171ee
Albert Cervera i Areny <albert@nan-tic.com>
2023-10-11 12:16:07 +0200

    Pyflakes fixes

--- purchase.py
+++ purchase.py
@@ -1,14 +1,11 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-from decimal import Decimal
 from trytond.model import fields
 from trytond.pool import PoolMeta
 from trytond.pyson import Eval
 from trytond.transaction import Transaction
 from trytond.i18n import gettext
 from trytond.exceptions import UserError
-
-__all__ = ['PurchaseLine', 'HandleShipmentException', 'HandleInvoiceException']
 
 
 class PurchaseLine(metaclass=PoolMeta):
