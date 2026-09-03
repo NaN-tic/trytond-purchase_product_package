4e7001b4296d2c12e88c795d47a310dcfb985cf7
Kevin Ortega Garcia <kevin.ortega@nan-tic.com>
2026-09-03 12:39:15 +0200

    Expanded purchase package functionality. package quantity is now a function field, that is calculated accordingly to quantity, or left empty if there is a mismatch. Purchase product supplier can hold a default package as well. Purchase requests can have a package too, that will be taken from the supplier by default. Purchase lines generated from requests from packages will use that same package. If stock_product_package is enabled on the environment, the package will be passed onto the moves generated from the purchase lines, as well. Task #186532

--- purchase.py
+++ purchase.py
@@ -1,7 +1,10 @@
+import math
+from decimal import Decimal
+
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 from trytond.model import fields
-from trytond.pool import PoolMeta
+from trytond.pool import Pool, PoolMeta
 from trytond.pyson import Eval
 from trytond.transaction import Transaction
 from trytond.i18n import gettext
@@ -30,15 +33,45 @@ class PurchaseLine(metaclass=PoolMeta):
             'required': Eval('product_has_packages', False),
             'readonly': Eval('purchase_state') != 'draft',
             })
-    package_quantity = fields.Integer('Package Quantity',
-        states={
-            'invisible': ~Eval('product_has_packages', False),
-            'required': Eval('product_has_packages', False),
-            'readonly': Eval('purchase_state') != 'draft',
-            })
+    package_quantity = fields.Function(fields.Integer('Package Quantity',
+            states={
+                'invisible': ~Eval('product_has_packages', False),
+                'required': Eval('product_has_packages', False),
+                'readonly': Eval('purchase_state') != 'draft',
+                }),
+        'on_change_with_package_quantity', setter='set_package_quantity')
 
-    @fields.depends('product_package', 'quantity', 'product_package',
-        'product', 'package_quantity')
+    @fields.depends('product_package', 'quantity', 'unit', 'product')
+    def on_change_with_package_quantity(self, name=None):
+        package_quantity = None
+        if self.product_package and self.quantity is not None:
+            package_unit = self.product_package.unit
+            if not package_unit and self.product:
+                package_unit = self.product.default_uom
+            if self.unit and package_unit:
+                Uom = Pool().get('product.uom')
+                quantity = Uom.compute_qty(
+                    self.unit, self.quantity, package_unit)
+                value = (Decimal(str(quantity)) /
+                    Decimal(str(self.product_package.quantity)))
+                if value == value.to_integral_value():
+                    package_quantity = int(value)
+        return package_quantity
+
+    @classmethod
+    def set_package_quantity(cls, lines, name, value):
+        to_write = []
+        for line in lines:
+            if not line.product_package or value is None:
+                continue
+            quantity = value * line.product_package.quantity
+            if line.unit:
+                quantity = round(float(quantity), line.unit.digits)
+            to_write.extend(([line], {'quantity': quantity}))
+        if to_write:
+            cls.write(*to_write)
+
+    @fields.depends('product_package', 'quantity', 'product', 'package_quantity')
     def pre_validate(self):
         try:
             super(PurchaseLine, self).pre_validate()
@@ -63,18 +96,10 @@ class PurchaseLine(metaclass=PoolMeta):
         super(PurchaseLine, self).on_change_product()
         self.product_package = None
         if self.product:
-            # Check if we have a product.package (product.product level)
-            for package in self.product.packages:
-                if package.is_default:
-                    self.product_package = package
-                    break
+            if self.product_supplier:
+                self.product_package = self.product_supplier.get_purchase_package()
             if not self.product_package:
-                # If we dont have a default value in (product.product) we try
-                # to search at template level (product.template)
-                for package in self.product.template.packages:
-                    if package.is_default:
-                        self.product_package = package
-                        break
+                self.product_package = self.product.get_purchase_package()
 
     @fields.depends('product', 'product_supplier')
     def on_change_with_product_has_packages(self, name=None):
@@ -98,19 +123,99 @@ class PurchaseLine(metaclass=PoolMeta):
     @fields.depends('product_package', 'package_quantity', 'quantity', 'unit',
         methods=['on_change_quantity', 'on_change_with_delivery_date'])
     def on_change_package_quantity(self):
-        if self.product_package and self.package_quantity:
+        if self.product_package and self.package_quantity is not None:
             self.quantity = round((float(self.package_quantity) *
                 self.product_package.quantity), self.unit.digits)
             self.on_change_quantity()
             self.amount = self.on_change_with_amount()
             self.delivery_date = self.on_change_with_delivery_date()
 
-    @fields.depends('product_package', 'quantity')
-    def on_change_quantity(self):
-        super(PurchaseLine, self).on_change_quantity()
-        if self.product_package and self.quantity:
-            self.package_quantity = int(self.quantity /
-                self.product_package.quantity)
+
+class PurchaseLineStockProductPackage(metaclass=PoolMeta):
+    __name__ = 'purchase.line'
+
+    def get_move(self, move_type):
+        move = super().get_move(move_type)
+        move.product_package = self.product_package
+        return move
+
+
+class PurchaseRequest(metaclass=PoolMeta):
+    __name__ = 'purchase.request'
+
+    product_package = fields.Many2One(
+        'product.package', 'Package',
+        domain=[
+            ['OR',
+                ('template.products', '=', Eval('product', 0)),
+                ('product', '=', Eval('product', 0)),
+                ],
+            ],
+        states={
+            'readonly': Eval('state') != 'draft',
+            },
+        depends=['product'])
+
+    @classmethod
+    def create(cls, vlist):
+        requests = super().create(vlist)
+        to_write = []
+        for request in requests:
+            if not request.product_package:
+                package = request._get_supplier_package()
+                if package:
+                    to_write.extend(([request], {
+                        'product_package': package.id,
+                        }))
+        if to_write:
+            cls.write(*to_write)
+        return requests
+
+    @fields.depends('product', 'party')
+    def on_change_product(self):
+        try:
+            super().on_change_product()
+        except AttributeError:
+            pass
+        self.product_package = self._get_supplier_package()
+
+    @fields.depends('product', 'party')
+    def on_change_party(self):
+        try:
+            super().on_change_party()
+        except AttributeError:
+            pass
+        self.product_package = self._get_supplier_package()
+
+    def _get_supplier_package(self):
+        if not self.product:
+            return None
+        for supplier in self.product.product_suppliers_used():
+            if supplier.party == self.party:
+                return supplier.get_purchase_package()
+
+class CreatePurchase(metaclass=PoolMeta):
+    __name__ = 'purchase.request.create_purchase'
+
+    def _group_purchase_line_key(self, request):
+        key = super()._group_purchase_line_key(request)
+        return key + (('product_package', request.product_package),)
+
+    @classmethod
+    def compute_purchase_line(cls, key, requests, purchase):
+        line = super().compute_purchase_line(key, requests, purchase)
+        package = requests[0].product_package
+        if package:
+            line.product_package = package
+            Uom = Pool().get('product.uom')
+            package_unit = (package.product.default_uom
+                if package.product else package.template.default_uom)
+            package_quantity = Uom.compute_qty(
+                package_unit, package.quantity, line.unit, round=False)
+            line.quantity = line.unit.ceil(
+                math.ceil(line.quantity / package_quantity)
+                * package_quantity)
+        return line
 
 
 class HandleShipmentException(metaclass=PoolMeta):
