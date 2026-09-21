932a9f7e73d10bd0485b29e03a8f731f1f9d60f7
KevinNantic <158449939+KevinNantic@users.noreply.github.com>
2026-09-21 10:36:14 +0200

    Revert "Purchase, purchase request and out move package overhaul after stock_…"
    This reverts commit 08253bbc0813af7946524579a8ba7bbc6ba6a41d.

--- __init__.py
+++ __init__.py
@@ -1,25 +1,16 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 from trytond.pool import Pool
-from . import product, purchase
+from . import product
+from . import purchase
 
 
 def register():
     Pool.register(
         product.Package,
-        product.Template,
-        product.Product,
-        product.ProductSupplier,
-        purchase.Configuration,
         purchase.PurchaseLine,
-        purchase.PurchaseRequest,
         module='purchase_product_package', type_='model')
     Pool.register(
-        purchase.PurchaseLineStockProductPackage,
-        depends=['stock_product_package'],
-        module='purchase_product_package', type_='model')
-    Pool.register(
-        purchase.CreatePurchase,
         purchase.HandleShipmentException,
         purchase.HandleInvoiceException,
         module='purchase_product_package', type_='wizard')
