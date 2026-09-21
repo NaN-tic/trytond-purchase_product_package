1cf125e6f5ee10b77ab01c4e724d1cb4cec6cb6b
nan-tic-dev <79672209+nan-tic-dev@users.noreply.github.com>
2026-09-21 10:35:08 +0200

    Purchase, purchase request and out move package overhaul after stock_product_package.
    * Expanded purchase package functionality. package quantity is now a function field, that is calculated accordingly to quantity, or left empty if there is a mismatch. Purchase product supplier can hold a default package as well. Purchase requests can have a package too, that will be taken from the supplier by default. Purchase lines generated from requests from packages will use that same package. If stock_product_package is enabled on the environment, the package will be passed onto the moves generated from the purchase lines, as well. Task #186532
    
    * Added package required for purchase lines option. Expanded tests and messages. Overall fixes. Added update script to set package required to true for environments with the module already installed. Task #186532
    
    ---------
    
    Co-authored-by: Kevin Ortega Garcia <kevin.ortega@nan-tic.com>

--- __init__.py
+++ __init__.py
@@ -1,16 +1,25 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 from trytond.pool import Pool
-from . import product
-from . import purchase
+from . import product, purchase
 
 
 def register():
     Pool.register(
         product.Package,
+        product.Template,
+        product.Product,
+        product.ProductSupplier,
+        purchase.Configuration,
         purchase.PurchaseLine,
+        purchase.PurchaseRequest,
         module='purchase_product_package', type_='model')
     Pool.register(
+        purchase.PurchaseLineStockProductPackage,
+        depends=['stock_product_package'],
+        module='purchase_product_package', type_='model')
+    Pool.register(
+        purchase.CreatePurchase,
         purchase.HandleShipmentException,
         purchase.HandleInvoiceException,
         module='purchase_product_package', type_='wizard')
