5ff66d393e52c93e4af0910cd6faac81bc3b2805
Raimon Esteve <raimonesteve@gmail.com>
2026-09-23 17:37:16 +0200

    FIX _TransactionLockError

--- update/186532.py
+++ update/186532.py
@@ -5,8 +5,11 @@ if 'pool' not in globals():
 
 
 Configuration = pool.get('purchase.configuration')
-configuration = Configuration(1)
-configuration.package_required = True
-configuration.save()
-
-transaction.commit()
+# Creating a missing singleton requires the table lock from the start of
+# the transaction. The console does not retry TransactionError exceptions.
+with transaction.new_transaction(
+        _lock_tables=[Configuration._table]) as configuration_transaction:
+    configuration = Configuration(1)
+    configuration.package_required = True
+    configuration.save()
+    configuration_transaction.commit()
